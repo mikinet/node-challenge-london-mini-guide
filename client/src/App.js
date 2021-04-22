10c1c74a16e4d6b46cf361e68967273d2ad3cb2b
Michael <antenehnigussie1@yahoo.com>
2021-04-22 08:48:25 +0100

    Added frontend and backend applications. Complted Levels 100 through 500 of the server-side challenge. Client-side application development work on progress.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,71 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Table from "./Table";
 
 function App() {
+  const [url, setUrl] = useState("");
+  const [data, setData] = useState([]);
+  const [cityName, setCityName] = useState("");
+
+  const tableHeader = ["#", "Name", "Phone", "Address", "Website"];
+  useEffect(() =>
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data[0]) {
+          data = data.map((item, index) => {
+            return {
+              id: index + 1,
+              name: item.name,
+              phone: item.phone,
+              address: item.address,
+              website: item.website,
+            };
+          });
+          return setData(data);
+        }
+      })
+  );
+  
+  const filterByDepartmentName = (event) => {
+    event.preventDefault();
+    const department = event.target.id;
+    const url = `http://localhost:5000/${cityName}/${department}`;
+    setUrl(url);
+  };
+  if (data[0]) {
+  }
   return (
     <div className="App">
       <h2>London Mini-Guide</h2>
+      <div>
+        <label for="cities"></label>
+        <select
+          id="cities"
+          onChange={(event) => setCityName(event.target.value)}
+        >
+          <option value="">Select a city...</option>
+          <option value="harrow">Harrow</option>
+          <option value="heathrow">Heathrow</option>
+          <option value="stratford">Stratford</option>
+        </select>
+      </div>
+      <div>
+        <button id="pharmacies" onClick={filterByDepartmentName}>
+          Pharmacies
+        </button>
+        <button id="colleges" onClick={filterByDepartmentName}>
+          Schools and Colleges
+        </button>
+        <button id="hospitals" onClick={filterByDepartmentName}>
+          Hospitals
+        </button>
+        <button id="doctors" onClick={filterByDepartmentName}>
+          Doctors
+        </button>
+      </div>
+
+      <Table labels={tableHeader} data={data} />
     </div>
   );
 }
---
+++ server/index.js
@@ -0,0 +1,47 @@
+/*********** ALL LEVELS *************/
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Your app is running on port ${PORT}.`));
+
+/************* LEVEL 100 ***************/
+// route "/"
+app.get("/", (req, res) => {
+  res.json({
+    "/colleges": "retruns an array of colleges in a specific area",
+    "/hospitals": "retruns an array of hospitals in a specific area",
+    "/doctors": "retruns an array of doctores in a specific area",
+    "/pharmcies": "retruns an array of pharmacies in a specific area",
+  });
+});
+
+/************* LEVELS 200 THROUGH 500 ***************/
+// IMPORT EVERY CITY/BURROUGH'S DATA 
+const HARROW = require("../data/Harrow.json");
+const HEATHROW = require("../data/Heathrow.json");
+const STRATFORD = require("../data/Stratford.json");
+
+app.get("/:city/:department", (req, res) => {
+  const city = req.params.city.toLowerCase(); // city = case-insensitive name of a burrough in London
+  const department = req.params.department.toLowerCase(); // department = case-insensitive name of a department
+  let cityData = [];
+  res.header({"Access-Control-Allow-Origin":"*"});
+  switch (city) {
+    case "harrow":
+      cityData = HARROW;
+      break;
+    case "heathrow":
+      cityData = HEATHROW;
+      break;
+    case "stratford":
+      cityData = STRATFORD;
+      break;
+    default:
+      cityData = [];
+  }
+  // now filter city JSON data by department name and send it to the client
+  res.json(cityData[department]);
+});
+
